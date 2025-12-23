1960881339aa0622872129cd80fff53c84785cc2
jonchao666 <zsc1406034465@gmail.com>
2025-12-23 10:25:37 +0900

    移除调试代码

--- index.js
+++ index.js
@@ -1003,17 +1003,10 @@ function startServer() {
 
       // 查询整体平均值
       const avgResult = db.exec(`
-        SELECT AVG(listener_count) as overallAvg, COUNT(*) as cnt
+        SELECT AVG(listener_count) as overallAvg
         FROM listeners
         WHERE timestamp >= ? AND timestamp < ?
       `, [startDate, endDate]);
-
-      console.log('Curve average debug:', {
-        type,
-        startDate,
-        endDate,
-        avgResult: avgResult.length > 0 ? avgResult[0].values : 'empty'
-      });
 
       const overallAvg = avgResult.length > 0 && avgResult[0].values.length > 0 && avgResult[0].values[0][0] !== null
         ? Math.round(avgResult[0].values[0][0] * 10) / 10
